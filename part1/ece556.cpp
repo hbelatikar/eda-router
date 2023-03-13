6d0ee7a69723db9bf041d314a91f217ab46caa46
hbelatikar <hrishikesh.belatikar@gmail.com>
2023-03-13 05:12:45 -0500

    Updated read function to read nets, edge utlisation remaining.

--- part1/ece556.cpp
+++ part1/ece556.cpp
@@ -17,22 +17,55 @@ int readBenchmark(const char *fileName, routingInst *rst){
 
       stream >> token;
       if (token == "grid") {
+        // Parse grid size and calculate num of edges
+
         stream >> rst->gx >> rst->gy;
         rst->numEdges = (rst->gx * (rst->gy - 1) + rst->gy * (rst->gx - 1));
-      } else if (token == "capacity") {
+        std::cout << "Grid WxH :" << rst->gx << "x" << rst->gy << std::endl;
+        std::cout << "Num of Edges :" << rst->numEdges << std::endl;        
+      }
+      else if (token == "capacity") {
+        // Parse the default capacity
+
         stream >> rst->cap;
-      } else if (token == "num") {
-        stream >> token >> rst->numNets; // Temporarily writing into "token" to ignore "net" from I/P file
+        std::cout << "Default Capacity :" << rst->cap << std::endl;
+      }
+      else if (token == "num") {
+        // Parse all the number of nets
+
+        stream >> token >> rst->numNets; 
         rst->nets = (net*)malloc(rst->numNets*sizeof(net));
-        for (uint32_t i = 0; i < rst->numNets; i++) {
+        
+        //Gather all net endpoints detail and net id
+        for (int i = 0; i < rst->numNets; i++) {
           std::getline(input_file,line);
           std::stringstream net_stream(line);
           if(line[0] == 'n') {
             net_stream >> token >> rst->nets[i].numPins;
-          } else {
-            for (uint32_t j = 0; j < rst->nets[i].numPins; j++)
+            rst->nets[i].id = stoi(token.substr(1,token.size()-1));
+            rst->nets[i].pins = (point*)malloc(rst->nets[i].numPins*sizeof(point));
+            for (int j = 0; j < rst->nets[i].numPins; j++) {
+              std::getline(input_file,line);
+              std::stringstream net_stream(line);
               net_stream >> rst->nets[i].pins[j].x >> rst->nets[i].pins[j].y;
+              std::cout << "Net : " << rst->nets[i].id << " Pin : ("<< rst->nets[i].pins[j].x <<","<<rst->nets[i].pins[j].y << ")\n";
+            }
+          } else {
+            std::cout << "ERROR: Could not parse net" << std::endl;
+            input_file.close();
+            return 0;
           }
+        }
+
+        //Read in the restricted edge capacities
+        std::getline(input_file,line);
+        std::stringstream net_stream(line);
+        if(line[0]=='n') {
+          std::cout << "ERROR: Observed more nets than expected" << std::endl;
+          input_file.close();
+          return 0;
+        } else {
+          return 1;
         }
       }
     }
