5363380fe593182759e9c3769cb9fc64aebcf65d
hbelatikar <hrishikesh.belatikar@gmail.com>
2023-04-17 01:37:49 -0500

    qsorting the nets during subnet generation

--- part2/ece556.cpp
+++ part2/ece556.cpp
@@ -291,13 +291,13 @@ int netDecompose (routingInst* rst) {
   // Iterate through all nets
   for(int i = 0; i < rst->numNets; i++){
     if (rst->nets[i].numPins > 2) {
-      // if(i&1) {
-      //   // If iterator is odd quicksort the nets according to the X axis
-      //   qsort(rst->nets[i].pins, rst->nets[i].numPins, sizeof(point), compareX);
-      // } else {
-      //   // Quicksort the nets according to the Y axis
-      //   qsort(rst->nets[i].pins, rst->nets[i].numPins, sizeof(point), compareY);
-      // }
+      if(i&1) {
+        // If iterator is odd quicksort the nets according to the X axis
+        qsort(rst->nets[i].pins, rst->nets[i].numPins, sizeof(point), compareX);
+      } else {
+        // Quicksort the nets according to the Y axis
+        qsort(rst->nets[i].pins, rst->nets[i].numPins, sizeof(point), compareY);
+      }
       
       // BRUTE FORCE THE SOLUTION
       // TODO: Find a better approach than Bubble
